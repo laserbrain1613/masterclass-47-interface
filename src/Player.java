3e7db03489aef8be02071a8eea7009d091ff1229
Lars Olani <lars.olani@gmail.com>
2021-02-17 17:03:25 +0100

    Initial commit

---
+++ src/Player.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player implements ISaveable {
+
+    private String name;
+    private String weapon;
+    private int hitPoints;
+    private int strength;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.weapon = "Sword";
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public List<String> write() {
+        List<String> playerStats = new ArrayList<>();
+        playerStats.add(this.name);
+        playerStats.add(String.valueOf(this.hitPoints));
+        playerStats.add(String.valueOf(this.strength));
+        playerStats.add(this.weapon);
+        return playerStats;
+    }
+
+    @Override
+    public void read(List<String> values) {
+        if (values != null && values.size() > 0) {
+            this.name = values.get(0);
+            this.hitPoints = Integer.parseInt(values.get(1));
+            this.strength = Integer.parseInt(values.get(2));
+            this.weapon = values.get(3);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                ", weapon='" + weapon + '\'' +
+                '}';
+    }
+
+}
